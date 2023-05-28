431a325ba8c801f942c6ba5570dc731dd69a9976
Swarp <m.kleban@terawatt-group.com>
2023-05-28 16:24:27 +0300

    added local store

--- src/redux/slice/contactsSlice.js
+++ src/redux/slice/contactsSlice.js
@@ -1,22 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from '@reduxjs/toolkit';
 
-
 const initialStateContacts = {
-  items: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ],
+  items: [],
 };
 
-// const initialStateContacts = [
-//     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//   ];
+// const initialStateContacts = [];
 
 const contactsSlice = createSlice({
   name: 'contacts',
@@ -24,7 +13,7 @@ const contactsSlice = createSlice({
   reducers: {
     addContact: {
       reducer(state, action) {
-              return {
+        return {
           items: [...state.items, action.payload],
         };
       },
@@ -53,4 +42,3 @@ export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
 
 export const getContacts = state => state.contacts.items;
-
--- src/redux/store.js
+++ src/redux/store.js
@@ -13,10 +13,34 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from '../redux/slice/contactsSlice';
 import { filterReducer } from '../redux/slice/filterSplice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(persistConfig, contactsReducer),
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
